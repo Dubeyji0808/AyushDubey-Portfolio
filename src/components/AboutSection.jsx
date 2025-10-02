13103d131eada9c57da362d8f516e3634d948c93
Ayush <ayushdubey12131213@gmail.com>
2025-10-02 18:41:14 +0530

    added resume

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -32,7 +32,7 @@ I have focused on learning core web development skills and am excited to apply t
               </a>
 
               <a
-                href=""
+                href="https://drive.google.com/file/d/1BDVMOiHwPuEm1EtA0PjCa8q7V0qP0lWn/view?usp=share_link"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
                 Download CV
